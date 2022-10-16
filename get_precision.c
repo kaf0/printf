adfcb5a82f25f28fc2972e634bf5d76904d2d8de
“kaf0” <gertrudeseworh@gmail.com>
2022-10-16 00:01:23 -0700

    Task 10 fix

--- get_precision.c
+++ get_precision.c
@@ -1,26 +1,41 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
 
-int get_precision(char *s, char formatter)
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ *
+ * Return: Precision.
+ */
+int get_precision(const char *format, int *i, va_list list)
 {
-	int p, i;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-	p = 0;
-	for (i = 0; s[i] != formatter; i++)
+	if (format[curr_i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (s[i] == '.')
+		if (is_digit(format[curr_i]))
 		{
-			i++;
-			while (s[i] != formatter)
-			{
-				p = 10 * p + s[i] - '0';
-				i++;
-			}
-			if (p == 0)
-				return (-1);
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
 			break;
 		}
+		else
+			break;
 	}
-	return (p);
+
+	*i = curr_i - 1;
+
+	return (precision);
 }
