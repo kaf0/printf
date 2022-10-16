3d791ad1a3972739b2b280edc8b4191e3df1d6b0
“kaf0” <gertrudeseworh@gmail.com>
2022-10-16 00:51:20 -0700

    Revert "Task 10 fix"
    This reverts commit adfcb5a82f25f28fc2972e634bf5d76904d2d8de.

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,26 @@
+#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
-/**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
- * @list: list of arguments
- *
- * Return: Precision.
- */
-int get_precision(const char *format, int *i, va_list list)
+int get_precision(char *s, char formatter)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+	int p, i;
 
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	p = 0;
+	for (i = 0; s[i] != formatter; i++)
 	{
-		if (is_digit(format[curr_i]))
+		if (s[i] == '.')
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
+			i++;
+			while (s[i] != formatter)
+			{
+				p = 10 * p + s[i] - '0';
+				i++;
+			}
+			if (p == 0)
+				return (-1);
 			break;
 		}
-		else
-			break;
 	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+	return (p);
 }
